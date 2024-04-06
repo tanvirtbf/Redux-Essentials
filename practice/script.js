c48ca21b1cd396cc51af7a1f3a8be1d5af68d053
tanvirtbf <tanver018765@gmail.com>
2024-04-06 09:59:26 +0600

    mutating state with simple example

---
+++ practice/script.js
@@ -0,0 +1,22 @@
+let state = {
+  count: 0,
+};
+
+// state.count = state.count + 1;
+// state.count = state.count + 1;
+// state.count = state.count + 1;
+// state.count = state.count + 1;
+
+function increment() {
+  state.count = state.count + 1;
+}
+increment()
+console.log(state);
+increment()
+console.log(state);
+increment()
+console.log(state);
+increment()
+console.log(state);
+increment()
+console.log(state);
