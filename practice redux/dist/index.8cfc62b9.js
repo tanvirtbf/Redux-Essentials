c72a9f8f1909470039ebd8e96b0848c4215d03df
tanvirtbf <tanver018765@gmail.com>
2024-04-08 11:06:23 +0600

    implement cart item increase and decrease quantity

--- practice redux/dist/index.8cfc62b9.js
+++ practice redux/dist/index.8cfc62b9.js
@@ -589,10 +589,12 @@ var _productsList = require("./productsList");
 const initState = {
     products: (0, _productsList.productsList),
     cartItems: [],
-    wishLists: []
+    wishList: []
 };
-const CART_ADD_ITEM = "cart/add";
-const CART_REMOVE_ITEM = "cart/remove";
+const CART_ADD_ITEM = "cart/addItem";
+const CART_REMOVE_ITEM = "cart/removeItem";
+const CART_ITEM_INCREASE_QUANTITY = "cart/increaseItemQuantity";
+const CART_ITEM_DECREASE_QUANTITY = "cart/decreaseItemQuantity";
 function reducer(state = initState, action) {
     switch(action.type){
         case CART_ADD_ITEM:
@@ -608,6 +610,28 @@ function reducer(state = initState, action) {
                 ...state,
                 cartItems: state.cartItems.filter((cartItem)=>cartItem.productId !== action.payload.productId)
             };
+        case CART_ITEM_INCREASE_QUANTITY:
+            return {
+                ...state,
+                cartItems: state.cartItems.map((cartItem)=>{
+                    if (cartItem.productId === action.payload.productId) return {
+                        ...cartItem,
+                        quantity: cartItem.quantity + 1
+                    };
+                    return cartItem;
+                })
+            };
+        case CART_ITEM_DECREASE_QUANTITY:
+            return {
+                ...state,
+                cartItems: state.cartItems.map((cartItem)=>{
+                    if (cartItem.productId === action.payload.productId) return {
+                        ...cartItem,
+                        quantity: cartItem.quantity - 1
+                    };
+                    return cartItem;
+                })
+            };
         default:
             return state;
     }
@@ -618,78 +642,55 @@ store.dispatch({
     type: CART_ADD_ITEM,
     payload: {
         productId: 1,
+        quantity: 5
+    }
+});
+store.dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+        productId: 12,
         quantity: 1
     }
 });
 store.dispatch({
     type: CART_ADD_ITEM,
     payload: {
-        productId: 2,
+        productId: 15,
         quantity: 1
     }
 });
 store.dispatch({
     type: CART_ADD_ITEM,
     payload: {
-        productId: 3,
-        quantity: 1
-    }
-});
-store.dispatch({
-    type: CART_ADD_ITEM,
-    payload: {
-        productId: 4,
+        productId: 6,
         quantity: 1
     }
 });
 store.dispatch({
     type: CART_REMOVE_ITEM,
     payload: {
-        productId: 2
+        productId: 6
     }
 });
 store.dispatch({
-    type: CART_REMOVE_ITEM,
+    type: CART_ITEM_INCREASE_QUANTITY,
     payload: {
-        productId: 4
+        productId: 12
     }
-}) // import { createStore } from "redux";
- // import { productsList } from "./productsList";
- // const initState = {
- //   products: productsList,
- //   cartItems: [],
- //   wishList: [],
- // };
- // const CART_ADD_ITEM = "cart/addItem";
- // const CART_REMOVE_ITEM = "cart/removeItem";
- // function reducer(state = initState, action) {
- //   switch (action.type) {
- //     case CART_ADD_ITEM:
- //       return { ...state, cartItems: [...state.cartItems, action.payload] };
- //     case CART_REMOVE_ITEM:
- //       return {
- //         ...state,
- //         cartItems: state.cartItems.filter(
- //           (cartItem) => cartItem.productId !== action.payload.productId
- //         ),
- //       };
- //     default:
- //       return state;
- //   }
- // }
- // const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
- // console.log(store);
- // store.dispatch({ type: CART_ADD_ITEM, payload: { productId: 1, quantity: 1 } });
- // store.dispatch({
- //   type: CART_ADD_ITEM,
- //   payload: { productId: 12, quantity: 1 },
- // });
- // store.dispatch({
- //   type: CART_ADD_ITEM,
- //   payload: { productId: 15, quantity: 1 },
- // });
- // store.dispatch({ type: CART_REMOVE_ITEM, payload: { productId: 12 } });
-;
+});
+store.dispatch({
+    type: CART_ITEM_INCREASE_QUANTITY,
+    payload: {
+        productId: 12
+    }
+});
+store.dispatch({
+    type: CART_ITEM_DECREASE_QUANTITY,
+    payload: {
+        productId: 1
+    }
+});
+console.log(store.getState());
 
 },{"redux":"anWnS","./productsList":"e6lBk"}],"anWnS":[function(require,module,exports) {
 // src/utils/formatProdErrorMessage.ts
--- practice redux/script.js
+++ practice redux/script.js
@@ -4,80 +4,80 @@ import { productsList } from "./productsList";
 const initState = {
   products: productsList,
   cartItems: [],
-  wishLists: [],
-}
+  wishList: [],
+};
 
-const CART_ADD_ITEM = 'cart/add'
-const CART_REMOVE_ITEM = 'cart/remove'
+const CART_ADD_ITEM = "cart/addItem";
+const CART_REMOVE_ITEM = "cart/removeItem";
+const CART_ITEM_INCREASE_QUANTITY = "cart/increaseItemQuantity";
+const CART_ITEM_DECREASE_QUANTITY = "cart/decreaseItemQuantity";
 
-function reducer(state=initState, action){
-  switch(action.type){
-    case CART_ADD_ITEM : 
-      return {...state, cartItems: [...state.cartItems, action.payload]}
-    case CART_REMOVE_ITEM : 
-      return {...state, cartItems: state.cartItems.filter((cartItem) => cartItem.productId !== action.payload.productId)}
-    default : 
+function reducer(state = initState, action) {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      return { ...state, cartItems: [...state.cartItems, action.payload] };
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.productId !== action.payload.productId
+        ),
+      };
+    case CART_ITEM_INCREASE_QUANTITY:
+      return {
+        ...state,
+        cartItems: state.cartItems.map((cartItem) => {
+          if (cartItem.productId === action.payload.productId) {
+            return { ...cartItem, quantity: cartItem.quantity + 1 };
+          }
+          return cartItem;
+        }),
+      };
+    case CART_ITEM_DECREASE_QUANTITY:
+      return {
+        ...state,
+        cartItems: state.cartItems.map((cartItem) => {
+          if (cartItem.productId === action.payload.productId) {
+            return { ...cartItem, quantity: cartItem.quantity - 1 };
+          }
+          return cartItem;
+        }),
+      };
+    default:
       return state;
   }
 }
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.())
-console.log(store)
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
 
+console.log(store);
 
-store.dispatch({type: CART_ADD_ITEM, payload: {productId: 1, quantity: 1}})
-store.dispatch({type: CART_ADD_ITEM, payload: {productId: 2, quantity: 1}})
-store.dispatch({type: CART_ADD_ITEM, payload: {productId: 3, quantity: 1}})
-store.dispatch({type: CART_ADD_ITEM, payload: {productId: 4, quantity: 1}})
-store.dispatch({type: CART_REMOVE_ITEM, payload: {productId: 2}})
-store.dispatch({type: CART_REMOVE_ITEM, payload: {productId: 4}})
+store.dispatch({ type: CART_ADD_ITEM, payload: { productId: 1, quantity: 5 } });
+store.dispatch({
+  type: CART_ADD_ITEM,
+  payload: { productId: 12, quantity: 1 },
+});
+store.dispatch({
+  type: CART_ADD_ITEM,
+  payload: { productId: 15, quantity: 1 },
+});
+store.dispatch({
+  type: CART_ADD_ITEM,
+  payload: { productId: 6, quantity: 1 },
+});
+store.dispatch({ type: CART_REMOVE_ITEM, payload: { productId: 6 } });
 
+store.dispatch({
+  type: CART_ITEM_INCREASE_QUANTITY,
+  payload: { productId: 12 },
+});
+store.dispatch({
+  type: CART_ITEM_INCREASE_QUANTITY,
+  payload: { productId: 12 },
+});
+store.dispatch({
+  type: CART_ITEM_DECREASE_QUANTITY,
+  payload: { productId: 1 },
+});
 
-
-
-
-
-
-
-// import { createStore } from "redux";
-// import { productsList } from "./productsList";
-
-// const initState = {
-//   products: productsList,
-//   cartItems: [],
-//   wishList: [],
-// };
-
-// const CART_ADD_ITEM = "cart/addItem";
-// const CART_REMOVE_ITEM = "cart/removeItem";
-
-// function reducer(state = initState, action) {
-//   switch (action.type) {
-//     case CART_ADD_ITEM:
-//       return { ...state, cartItems: [...state.cartItems, action.payload] };
-//     case CART_REMOVE_ITEM:
-//       return {
-//         ...state,
-//         cartItems: state.cartItems.filter(
-//           (cartItem) => cartItem.productId !== action.payload.productId
-//         ),
-//       };
-//     default:
-//       return state;
-//   }
-// }
-
-// const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
-
-// console.log(store);
-
-// store.dispatch({ type: CART_ADD_ITEM, payload: { productId: 1, quantity: 1 } });
-// store.dispatch({
-//   type: CART_ADD_ITEM,
-//   payload: { productId: 12, quantity: 1 },
-// });
-// store.dispatch({
-//   type: CART_ADD_ITEM,
-//   payload: { productId: 15, quantity: 1 },
-// });
-// store.dispatch({ type: CART_REMOVE_ITEM, payload: { productId: 12 } });
+console.log(store.getState());
