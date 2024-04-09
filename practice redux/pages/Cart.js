f92aeb490f31381406016d85f16dce77bbe19312
tanvirtbf <tanver018765@gmail.com>
2024-04-09 21:44:36 +0600

    complete cartPage basic functionality

--- practice redux/pages/Cart.js
+++ practice redux/pages/Cart.js
@@ -3,35 +3,9 @@ import CartItem from "../components/CartItem";
 import { useSelector } from "react-redux";
 
 export default function Cart() {
-  const cartItems = [
-    {
-      id: 1,
-      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-      imageUrl: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-      quantity: 1,
-      rating: 3.9,
-      price: 109.95,
-    },
-    {
-      id: 2,
-      title: "Mens Cotton Jacket",
-      imageUrl: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-      quantity: 1,
-      rating: 4.7,
-      price: 55.99,
-    },
-    {
-      id: 3,
-      title: "Mens Casual Slim Fit",
-      imageUrl: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-      quantity: 1,
-      rating: 2.1,
-      price: 15.99,
-    },
-  ];
 
-  const cart = useSelector((state) => state.cartItems)
-  console.log(cart)
+  const cartItems = useSelector((state) => state.cartItems)
+  console.log(cartItems)
 
   return (
     <div className="cart-container">
@@ -43,9 +17,9 @@ export default function Cart() {
           <div className="quantity">Quantity</div>
           <div className="total">Total</div>
         </div>
-        {cartItems.map(({ id, title, rating, price, imageUrl, quantity }) => (
+        {cartItems.map(({ productId, title, rating, price, imageUrl, quantity }) => (
           <CartItem
-            key={id}
+            key={productId}
             title={title}
             price={price}
             quantity={quantity}
