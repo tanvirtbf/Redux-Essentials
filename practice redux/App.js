9c8c0c06376eef948022c3ec88b3e422b1d3115a
tanvirtbf <tanver018765@gmail.com>
2024-04-09 17:25:40 +0600

    create header and cart component and implement react router functionality in this project and adding more file

--- practice redux/App.js
+++ practice redux/App.js
@@ -1,15 +1,14 @@
 import React from 'react'
+import Header from './components/Header'
+import { Outlet } from 'react-router-dom'
+
 import './App.css'
-import Product from './components/Product'
-import { useSelector } from 'react-redux'
 
 export default function App() {
-  const productsList = useSelector((state) => state.products)
   return (
-    <div className='products-container'>
-      {
-        productsList.map(({id, title, rating, price, image}) => <Product key={id} title={title} rating={rating} price={price} imageUrl={image}/>)
-      }
-    </div>
+    <>
+      <Header />
+      <Outlet />
+    </>
   )
 }
