9d84bb1bddcdd0288dc6f6f9cbddaa82220f9ac2
tanvirtbf <tanver018765@gmail.com>
2024-04-09 14:28:02 +0600

    complete alone

--- practice redux/productsReducer.js
+++ practice redux/productsReducer.js
@@ -1,4 +1,6 @@
-
+export default function productsReducer(state=[], action){
+  return state;
+}
 
 
 
--- practice redux/script.js
+++ practice redux/script.js
@@ -1,71 +1,34 @@
-import { createStore } from "redux";
-import { productsList } from "./productsList";
+import { combineReducers, createStore } from "redux";
+import productsReducer from "./productsReducer";
+import wishListReducer, { wishListAddItem, wishListRemoveItem } from "./wishListReducer";
+import cartReducer, { cartAddItem, cartdecreaseItem, cartIncreaseItem, cartRemoveItem } from './cartReducer'
 
-const initState = {
-  products: productsList,
-  cartItems: [],
-  wishLists: [],
-}
-
-const CART_ADD_ITEM = 'cart/addItem'
-const CART_REMOVE_ITEM = 'cart/removeItem'
-const CART_ITEM_INCREASE_QUANTITY = 'cart/increaseItem'
-const CART_ITEM_DECREASE_QUANTITY = 'cart/decreaseItem'
-const WISHLIST_ADD_ITEM = 'wishlist/addItem'
-const WISHLIST_REMOVE_ITEM = 'wishlist/removeItem'
-
-function reducer(state=initState, action){
-  switch(action.type){
-    case CART_ADD_ITEM: 
-      return {...state, cartItems: [...state.cartItems, action.payload]}
-    case CART_REMOVE_ITEM: 
-      return {...state, cartItems: state.cartItems.filter((cartItem) => cartItem.productId !== action.payload.productId)}
-    case CART_ITEM_INCREASE_QUANTITY: 
-      return {...state, cartItems: state.cartItems.map((cartItem) => {
-        if(cartItem.productId === action.payload.productId){
-          return {...cartItem, quantity: cartItem.quantity + action.payload.quantity}
-        }
-        return cartItem
-      })}
-    case CART_ITEM_DECREASE_QUANTITY: 
-      return {...state, cartItems: state.cartItems.map((cartItem) => {
-        if(cartItem.productId === action.payload.productId){
-          return {...cartItem, quantity: cartItem.quantity - action.payload.quantity}
-        }
-        return cartItem
-      }).filter((item) => item.quantity > 0)}
-    case WISHLIST_ADD_ITEM: 
-      return {...state, wishLists: [...state.wishLists, action.payload]}
-    case WISHLIST_REMOVE_ITEM: 
-      return {...state, wishLists: state.wishLists.filter((wishlistItem) => wishlistItem.productId !== action.payload.productId)}
-    default: 
-      return state;
-  }
-}
-
+const reducer = combineReducers({
+  products: productsReducer,
+  cartItems: cartReducer,
+  wishLists: wishListReducer,
+})
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.())
 
 console.log(store)
 
-store.dispatch({type: CART_ADD_ITEM, payload: {productId: 1, quantity: 2}})
-store.dispatch({type: CART_ADD_ITEM, payload: {productId: 2, quantity: 3}})
-store.dispatch({type: CART_ADD_ITEM, payload: {productId: 3, quantity: 4}})
-store.dispatch({type: CART_ADD_ITEM, payload: {productId: 4, quantity: 7}})
+store.dispatch(cartAddItem(1,2))
+store.dispatch(cartAddItem(2,3))
+store.dispatch(cartAddItem(3,5))
+store.dispatch(cartAddItem(4,8))
 
-store.dispatch({type: CART_REMOVE_ITEM, payload: {productId: 2}})
+store.dispatch(cartRemoveItem(3))
 
-store.dispatch({type: CART_ITEM_INCREASE_QUANTITY, payload: {productId: 1, quantity: 1}})
-store.dispatch({type: CART_ITEM_INCREASE_QUANTITY, payload: {productId: 4, quantity: 2}})
+store.dispatch(cartIncreaseItem(2,3))
 
-store.dispatch({type: CART_ITEM_DECREASE_QUANTITY, payload: {productId: 4, quantity: 10}})
+store.dispatch(cartdecreaseItem(2,4))
 
-store.dispatch({type: WISHLIST_ADD_ITEM, payload: {productId: 1, quantity: 1}})
-store.dispatch({type: WISHLIST_ADD_ITEM, payload: {productId: 2, quantity: 1}})
+store.dispatch(wishListAddItem(1,2))
+store.dispatch(wishListAddItem(2,4))
+store.dispatch(wishListAddItem(3,1))
 
-store.dispatch({type: WISHLIST_REMOVE_ITEM, payload: {productId: 1}})
-
-
+store.dispatch(wishListRemoveItem(2))
 
 
 
