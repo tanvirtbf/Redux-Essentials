363bf5d12a1ec110d18fe4a1d52d9a94f4cfb41f
tanvirtbf <tanver018765@gmail.com>
2024-04-10 11:25:46 +0600

    bug fixed and implement total cart number in header

--- practice redux/components/Header.js
+++ practice redux/components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import CartIcon from "../assets/cart-icon.svg";
-import { useSelector } from "react-redux";
 
 export default function Header() {
-  const cartItems = useSelector((state) => state.cartItems)
+  const cartItems = useSelector((state) => state.cartItems);
   return (
     <header>
       <div className="header-contents">
@@ -13,7 +13,9 @@ export default function Header() {
         </h1>
         <Link className="cart-icon" to="/cart">
           <img src={CartIcon} alt="cart-icon" />
-          <div className="cart-items-count">0</div>
+          <div className="cart-items-count">
+            {cartItems.reduce((prev,curr) => prev + curr.quantity , 0)}
+          </div>
         </Link>
       </div>
     </header>
